922f2c1b529d8fe6a02f43e9eb8acb6f52689a71
Frendty <federicoragusa@hotmail.com>
2024-01-11 21:20:15 +0100

    Sostituito raise exception con stringa di errore di ritorno

--- Volpe_Ragusa/python/user_manager.py
+++ Volpe_Ragusa/python/user_manager.py
@@ -3,7 +3,7 @@ from utils import connect_go_server, is_valid_email, is_valid_password
 # (account) deve essere dizionario con i campi 'email' e 'password'
 def login(account):
     if type(account) is not dict:
-        raise TypeError(f"Errore login: account deve essere un dizionario, invece è di tipo {type(account)}")
+        return f"Errore login: account deve essere un dizionario, invece è di tipo {type(account)}"
     
     if is_valid_email(account['email']) and is_valid_password(account['password']):
         try:
@@ -16,7 +16,7 @@ def login(account):
 # In questo caso account deve essere un dizionario con i campi 'email', 'password', 'name', 'surname'
 def signin(account):
     if type(account) is not dict:
-        raise TypeError(f"Errore signin: account deve essere un dizionario, invece è di tipo {type(account)}")
+        return f"Errore signin: account deve essere un dizionario, invece è di tipo {type(account)}"
     
     if is_valid_email(account['email']) and is_valid_password(account['password']) and account['name'] != '' and account['surname'] != '':
         try:
