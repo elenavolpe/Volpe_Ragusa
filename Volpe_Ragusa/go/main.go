a3a213b3739496b53a2da2daac1a0bba9a964444
FrendtyLaptop <federicoragusa@hotmail.com>
2024-01-12 17:23:28 +0100

    Server go base implementato

--- Volpe_Ragusa/go/main.go
+++ Volpe_Ragusa/go/main.go
@@ -1,8 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Definizione Handlers
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home page")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About us page")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contact us")
+}
 
 func main() {
-	name := "Go Developers"
-	fmt.Println("Azure for", name)
+	mux := http.NewServeMux()
+
+	// Dichiarazione handlers richieste per le diverse routes gestite dal multiplexer/router mux
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/about", aboutHandler)
+	mux.HandleFunc("/contact", contactHandler)
+
+	// Porta del server
+	port := 8080
+
+	// Definizione server personalizzato che fa uso come handler del multiplexer/router mux
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: mux,
+	}
+
+	// Avvio del server
+	fmt.Printf("Server is listening on port %d...\n", port)
+	log.Fatal(server.ListenAndServe())
 }
