b6128b7f83552eb199d46257fbf2f670fbd3d697
elenavolpe <VLPLNE00S50A028W@studium.unict.it>
2024-01-16 23:40:58 +0100

    inserite get esercizi nella home, da sistemare

--- Volpe_Ragusa/csharp/Home.cs
+++ Volpe_Ragusa/csharp/Home.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -35,6 +38,126 @@ namespace Volpe_Ragusa.csharp
         {
             Form1 login=new Form1();
             login.Show();
+        }
+
+        //vedi come chiamare questa funzione
+        public void carica_esercizi()
+        {
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    // URL del server Python
+                    string url = "http://localhost:5000/get_esercizi"; //TO_DO da sistemare
+                    // Creazione dei dati da inviare come parte della richiesta POST
+                    NameValueCollection postData = new NameValueCollection
+                    {
+                        { "email", this.email }
+                        // Aggiungi altri parametri se necessario
+                    };
+                    // Invio della richiesta POST sincrona
+                    byte[] responseBytes = client.UploadValues(url, "POST", postData);
+                    // Converti la risposta in una stringa
+                    string responseBody = System.Text.Encoding.UTF8.GetString(responseBytes);
+                    // metto quello che torna nel vettore di esercizi
+                    string[] exercises = JsonConvert.DeserializeObject<string[]>(responseBody);
+                    // Aggiungi dinamicamente i controlli al form
+                    for (int i = 0; i < exercises.Length; i++)
+                    {
+                        Label label = new Label();
+                        //vediamo cosa mi torna il json, creo label nome e label descrizione
+                        //label.Text = $"Esercizio {i + 1}: {exercises[i]}";
+                        label.Location = new System.Drawing.Point(20, 50 + 30 * i);
+                        label.Size = new System.Drawing.Size(200, 20);
+                        Controls.Add(label);
+                    }
+                }
+                catch (WebException ex)
+                {
+                    // Gestisci eventuali errori durante la richiesta HTTP
+                    Console.WriteLine($"Errore durante la richiesta HTTP: {ex.Message}");
+                }
+            }
+        }
+
+        //vedi come chiamare questa funzione
+        public void carica_esercizi_preferiti()
+        {
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    // URL del server Python
+                    string url = "http://localhost:5000/get_esercizi_preferiti"; //TO_DO da sistemare
+                    // Creazione dei dati da inviare come parte della richiesta POST
+                    NameValueCollection postData = new NameValueCollection
+                    {
+                        { "email", this.email }
+                        // Aggiungi altri parametri se necessario
+                    };
+                    // Invio della richiesta POST sincrona
+                    byte[] responseBytes = client.UploadValues(url, "POST", postData);
+                    // Converti la risposta in una stringa
+                    string responseBody = System.Text.Encoding.UTF8.GetString(responseBytes);
+                    // metto quello che torna nel vettore di esercizi
+                    string[] exercises = JsonConvert.DeserializeObject<string[]>(responseBody);
+                    // Aggiungi dinamicamente i controlli al form
+                    for (int i = 0; i < exercises.Length; i++)
+                    {
+                        Label label = new Label();
+                        //vediamo cosa mi torna il json, creo label nome e label descrizione
+                        //label.Text = $"Esercizio {i + 1}: {exercises[i]}";
+                        label.Location = new System.Drawing.Point(20, 50 + 30 * i);
+                        label.Size = new System.Drawing.Size(200, 20);
+                        Controls.Add(label);
+                    }
+                }
+                catch (WebException ex)
+                {
+                    // Gestisci eventuali errori durante la richiesta HTTP
+                    Console.WriteLine($"Errore durante la richiesta HTTP: {ex.Message}");
+                }
+            }
+        }
+
+        //vedi come chiamare questa funzione
+        public void carica_esercizi_novità()
+        {
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    // URL del server Python
+                    string url = "http://localhost:5000/get_esercizi_recenti"; //TO_DO da sistemare
+                    // Creazione dei dati da inviare come parte della richiesta POST
+                    NameValueCollection postData = new NameValueCollection
+                    {
+                        { "email", this.email }
+                        // Aggiungi altri parametri se necessario
+                    };
+                    // Invio della richiesta POST sincrona
+                    byte[] responseBytes = client.UploadValues(url, "POST", postData);
+                    // Converti la risposta in una stringa
+                    string responseBody = System.Text.Encoding.UTF8.GetString(responseBytes);
+                    // metto quello che torna nel vettore di esercizi
+                    string[] exercises = JsonConvert.DeserializeObject<string[]>(responseBody);
+                    // Aggiungi dinamicamente i controlli al form
+                    for (int i = 0; i < exercises.Length; i++)
+                    {
+                        Label label = new Label();
+                        //vediamo cosa mi torna il json, creo label nome e label descrizione
+                        //label.Text = $"Esercizio {i + 1}: {exercises[i]}";
+                        label.Location = new System.Drawing.Point(20, 50 + 30 * i);
+                        label.Size = new System.Drawing.Size(200, 20);
+                        Controls.Add(label);
+                    }
+                }
+                catch (WebException ex)
+                {
+                    // Gestisci eventuali errori durante la richiesta HTTP
+                    Console.WriteLine($"Errore durante la richiesta HTTP: {ex.Message}");
+                }
+            }
         }
     }
 }
