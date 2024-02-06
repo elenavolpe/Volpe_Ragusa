9082514c7f1c0f1e1175ab48c13d945e0dbaab02
elenavolpe <VLPLNE00S50A028W@studium.unict.it>
2024-02-06 20:09:09 +0100

    account admin

--- Volpe_Ragusa/csharp/Account.Designer.cs
+++ Volpe_Ragusa/csharp/Account.Designer.cs
@@ -40,7 +40,16 @@
             buttonImpostazioni = new Button();
             label1 = new Label();
             flowLayoutPanel1 = new FlowLayoutPanel();
+            flowLayoutPanelAggiungi = new FlowLayoutPanel();
+            labelAggiungi = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            checkedListBox1 = new CheckedListBox();
             flowLayoutPanel1.SuspendLayout();
+            flowLayoutPanelAggiungi.SuspendLayout();
             SuspendLayout();
             // 
             // labelBenvenuto
@@ -148,16 +157,90 @@
             // flowLayoutPanel1
             // 
             flowLayoutPanel1.Controls.Add(label1);
-            flowLayoutPanel1.Location = new Point(18, 328);
+            flowLayoutPanel1.Location = new Point(18, 393);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(548, 113);
+            flowLayoutPanel1.Size = new Size(548, 102);
             flowLayoutPanel1.TabIndex = 11;
+            // 
+            // flowLayoutPanelAggiungi
+            // 
+            flowLayoutPanelAggiungi.Controls.Add(label2);
+            flowLayoutPanelAggiungi.Controls.Add(labelAggiungi);
+            flowLayoutPanelAggiungi.Controls.Add(textBox1);
+            flowLayoutPanelAggiungi.Controls.Add(label3);
+            flowLayoutPanelAggiungi.Controls.Add(checkedListBox1);
+            flowLayoutPanelAggiungi.Controls.Add(textBox2);
+            flowLayoutPanelAggiungi.Controls.Add(label4);
+            flowLayoutPanelAggiungi.Location = new Point(21, 78);
+            flowLayoutPanelAggiungi.Name = "flowLayoutPanelAggiungi";
+            flowLayoutPanelAggiungi.Size = new Size(545, 312);
+            flowLayoutPanelAggiungi.TabIndex = 12;
+            // 
+            // labelAggiungi
+            // 
+            labelAggiungi.AutoSize = true;
+            labelAggiungi.Location = new Point(3, 0);
+            labelAggiungi.Name = "labelAggiungi";
+            labelAggiungi.Size = new Size(59, 25);
+            labelAggiungi.TabIndex = 0;
+            labelAggiungi.Text = "Aggiungi esercizio";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(8, 42);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 38);
+            label2.TabIndex = 1;
+            label2.Text = "nome:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(4, 78);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(241, 31);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 121);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 38);
+            label3.TabIndex = 3;
+            label3.Text = "descrizione:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(-1, 148);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(529, 31);
+            textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 189);
+            label4.Name = "label4";
+            label4.Size = new Size(88, 25);
+            label4.TabIndex = 5;
+            label4.Text = "muscoli allenati";
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "addominali", "braccia", "gambe", "glutei", "quadricipiti" });
+            checkedListBox1.Location = new Point(16, 231);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(229, 60);
+            checkedListBox1.TabIndex = 6;
             // 
             // Account
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 495);
+            Controls.Add(flowLayoutPanelAggiungi);
             Controls.Add(flowLayoutPanel1);
             Controls.Add(buttonImpostazioni);
             Controls.Add(buttonLogout);
@@ -173,6 +256,8 @@
             Text = "Account";
             flowLayoutPanel1.ResumeLayout(false);
             flowLayoutPanel1.PerformLayout();
+            flowLayoutPanelAggiungi.ResumeLayout(false);
+            flowLayoutPanelAggiungi.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -191,5 +276,13 @@
         private Button buttonImpostazioni;
         private Label label1;
         private FlowLayoutPanel flowLayoutPanel1;
+        private FlowLayoutPanel flowLayoutPanelAggiungi;
+        private Label labelAggiungi;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private CheckedListBox checkedListBox1;
     }
 }
