ff7be8c65383312cf34e075783ab03853bff7b8d
Frendty <federicoragusa@hotmail.com>
2024-02-13 20:35:17 +0100

    .

--- Volpe_Ragusa/go/database/database.go
+++ Volpe_Ragusa/go/database/database.go
@@ -515,7 +515,7 @@ func EditExerciseName(old_name, new_name string, done chan<- bool) {
 	}
 	defer db.Close()
 
-	editQuery := "UPDATE exercises SET name = ? WHERE BINARY name = ?"
+	editQuery := "UPDATE exercises SET name = ? WHERE name = ?"
 	_, err = db.Exec(editQuery, new_name, old_name)
 	if err != nil {
 		log.Println(err)
